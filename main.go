f93ef08206eab49041cd40a5078ff3b4c03fe2b7
unknown <ahmedabdelgowadramadan@gmail.com>
2023-08-27 20:03:34 +0300

    add mongo db layer, create user store, handle some errors

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/godazz/Funduqi/api"
+	"github.com/godazz/Funduqi/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbname = "Funduqi-db"
+const userColl = "users"
+
+var config = fiber.Config{
+	// Override default error handler
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
+
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 }
