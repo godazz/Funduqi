75bc2101d6f105c60560c06cb632aefc59ab1fba
Ahmed Abd El Gawad <ahmedabdelgowadramadan@gmail.com>
2023-09-04 19:33:08 +0300

    Add testing setup for testing handlers

---
+++ api/user_handler_test.go
@@ -0,0 +1,83 @@
+package api
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/godazz/Funduqi/db"
+	"github.com/godazz/Funduqi/types"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	testdburi = "mongodb://localhost:27017"
+	dbname    = "Funduqi-db-test"
+)
+
+type testdb struct {
+	db.UserStore
+}
+
+func (tdb *testdb) teardown(t *testing.T) {
+	if err := tdb.UserStore.Drop(context.TODO()); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func setup(t *testing.T) *testdb {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(testdburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &testdb{
+		UserStore: db.NewMongoUserStore(client, dbname),
+	}
+}
+
+func TestPostUser(t *testing.T) {
+	tdb := setup(t)
+	defer tdb.teardown(t)
+
+	app := fiber.New()
+	userHandler := NewUserHandler(tdb.UserStore)
+	app.Post("/", userHandler.HandlePostUser)
+
+	params := types.CreateUserParams{
+		FirstName: "goda",
+		LastName:  "Ahmed",
+		Email:     "goda@gmail.com",
+		Password:  "gdfhgkjdfhkjdhfsd",
+	}
+
+	b, _ := json.Marshal(params)
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var user types.User
+	json.NewDecoder(resp.Body).Decode(&user)
+	if len(user.ID) == 0 {
+		t.Errorf("expecting a user ID")
+	}
+	if len(user.EncryptedPassword) > 0 {
+		t.Errorf("expected no return of encrypted password")
+	}
+	if user.FirstName != params.FirstName {
+		t.Errorf("expected firstname %s but got %s", params.FirstName, user.FirstName)
+	}
+	if user.LastName != params.LastName {
+		t.Errorf("expected lastname %s but got %s", params.LastName, user.LastName)
+	}
+	if user.Email != params.Email {
+		t.Errorf("expected email %s but got %s", params.Email, user.Email)
+	}
+}
